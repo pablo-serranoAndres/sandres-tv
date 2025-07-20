d827e988a45a3030dba01c4a5c167718938d8a2a
pserranoItaca <0419pserrano@e-itaca.es>
2025-07-20 19:16:17 +0200

    feat: updated index.js to listen 0.0.0.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`server runin in puerto${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor corriendo en puerto 3000");
 });
